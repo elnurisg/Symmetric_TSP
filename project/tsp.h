78fc9b841e9a47c0ea4b064e677f1a186305c3c7
Elnur Isgandarov <elnur1isgenderov@gmail.com>
2023-11-05 00:58:58 +0100

    tsp.h update

--- project/tsp.h
+++ project/tsp.h
@@ -36,13 +36,15 @@ typedef struct {
 	double timelimit;
 
 	//global data
-	double	tstart;								
-	double zbest;							// best sol. available  
-	double tbest;							// time for the best sol. available  
+	double *uncovered_nodes;
+	double best_val;
+	// double	tstart;								
+	// double zbest;							// best sol. available  
+	// double tbest;							// time for the best sol. available  
 	double *best_sol;						// best sol. available    
-	double	best_lb;						// best lower bound available  
-	double *load_min;						// minimum load when leaving a node
-	double *load_max;						// maximum load when leaving a node
+	// double	best_lb;						// best lower bound available  
+	// double *load_min;						// minimum load when leaving a node
+	// double *load_max;						// maximum load when leaving a node
 
 } instance;        
 
@@ -51,5 +53,5 @@ inline int imax(int i1, int i2) { return ( i1 > i2 ) ? i1 : i2; }
 inline double dmin(double d1, double d2) { return ( d1 < d2 ) ? d1 : d2; } 
 inline double dmax(double d1, double d2) { return ( d1 > d2 ) ? d1 : d2; } 
 int dist(int i, int j, instance *inst);
-
+int greedy_heuristic(instance *inst);
 #endif   /* TSP_H_ */ 
