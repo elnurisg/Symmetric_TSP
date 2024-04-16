3837ac620531e732d5fe64b9121b8d930e6cc9fd
Elnur Isgandarov <elnur1isgenderov@gmail.com>
2024-04-17 01:21:04 +0200

    Adding Local Branching

--- project/matheuristics.c
+++ project/matheuristics.c
@@ -2,6 +2,9 @@
 
 double second();  
 
+
+/////////////////////////////////// Hard-fixing /////////////////////////////////////////
+
 int hard_fixing(instance *inst, double pfix){
 
     if (inst->timelimit == CPX_INFBOUND)
@@ -22,13 +25,13 @@ int hard_fixing(instance *inst, double pfix){
 	if ( two_opt_refining_heuristic(inst, inst->best_sol, 0) ) print_error(" error within two_opt_refining_heuristic()");
 	calculate_best_val(inst);
     
-    double initial_best_value = inst->best_val;
+    double absolute_best_value = inst->best_val;
     while (second() - inst->tstart < inst->timelimit)
     {
         fixing(env, lp, inst, pfix);
         if (branch_and_cut(inst, env, lp)) print_error("Error in branch_and_cut()");
         unfixing(env, lp, inst);
-        if(VERBOSE >= 100 && initial_best_value > inst->best_val)    printf("Found better solution with value: %f\n", inst->best_val);
+        if(VERBOSE >= 100 && absolute_best_value > inst->best_val)    printf("Found better solution with value: %f\n", inst->best_val);
     }
     
 
@@ -85,5 +88,98 @@ void unfixing(CPXENVptr env, CPXLPptr lp, instance *inst){
 
         }
     }
+
+}
+
+
+/////////////////////////////////// Local branching /////////////////////////////////////////
+
+int local_branching(instance *inst, int k){
+    
+    if (inst->timelimit == CPX_INFBOUND)
+        print_error("Time limit not specified or invalid. \nFor Local Branching method you should specify the time limit as the goal is to minimize the primal integral in given time limit.\n");
+
+    // open CPLEX model
+    int error;
+    CPXENVptr env = CPXopenCPLEX(&error);
+    if ( error ) print_error("CPXopenCPLEX() error");
+    CPXLPptr lp = CPXcreateprob(env, &error, "TSP model version 1"); 
+    if ( error ) print_error("CPXcreateprob() error");
+
+    build_model(inst, env, lp);
+
+    inst->ncols = CPXgetnumcols(env, lp);
+
+    if ( greedy_heuristic(inst, 2, 0) ) print_error(" error within greedy_heuristic()");
+    if ( two_opt_refining_heuristic(inst, inst->best_sol, 0) ) print_error(" error within two_opt_refining_heuristic()");
+    calculate_best_val(inst);
+    
+    double absolute_best_value = inst->best_val;
+    int k_initial = k;
+
+    while (second() - inst->tstart < inst->timelimit)
+    {
+        add_local_branching_constraints(env, lp, inst, k);
+        if (branch_and_cut(inst, env, lp)) print_error("Error in branch_and_cut()");
+        remove_last_constraints(env, lp);
+
+        if(absolute_best_value <= inst->best_val){
+            k = 2*k; // if stuck in a local minimum, increase the size of the neighborhood
+            if(VERBOSE >= 100)    printf("Stuck in a local minimum with value: %f\n\t[!]So k is increased twice (k=%d) for the next iteration\n", inst->best_val, k);
+        }
+        else {
+            k = k_initial; // if found a better solution, reset the size of the neighborhood
+            if(VERBOSE >= 100)    printf("Found better solution with value: %f\n", inst->best_val);
+            absolute_best_value = inst->best_val;
+        }
+    }
+    
+
+    // free and close cplex model   
+    CPXfreeprob(env, &lp);
+    CPXcloseCPLEX(&env); 
+
+    return 0; // or an appropriate nonzero error code
+}
+
+void add_local_branching_constraints(CPXENVptr env, CPXLPptr lp, instance *inst, int k){
+    
+    int *succ = (int *) calloc(inst->nnodes, sizeof(int));
+    store_succ(inst, succ, inst->best_sol);
+
+	int *index = (int *) calloc(inst->ncols, sizeof(int));
+	double *value = (double *) calloc(inst->ncols, sizeof(double));
+	double rhs = inst->nnodes - k;
+	char sense = 'G';                            // 'G' for greater or equal
+	int nnz = 0;
+
+	for ( int i = 0; i < inst->nnodes; i++ )
+	{
+        for (int j = i+1; j < inst->nnodes; j++)
+        {
+            if (j == succ[i] || i == succ[j]) // if Xij = 1
+            {
+                index[nnz] = xpos(i,j, inst);
+                value[nnz] = 1.0;
+                nnz++;
+            }
+        }
+	}
+	int izero = 0;
+
+	if ( CPXaddrows(env, lp, 0, 1, nnz, &rhs, &sense, &izero, index, value, NULL, NULL) ) print_error("CPXaddrows(): error");
+	
+    free(succ);
+	free(value);
+	free(index);
+
+}
+
+void remove_last_constraints(CPXENVptr env, CPXLPptr lp){
+        
+    int begin = CPXgetnumrows(env, lp) - 1;
+    int end = CPXgetnumrows(env, lp) - 1;
+
+    if (CPXdelrows(env, lp, begin, end)) print_error("Error in CPXdelrows()");
 
 }
--- project/matheuristics.h
+++ project/matheuristics.h
@@ -1,5 +1,14 @@
 #include "tsp.h"
 
+/////////////////////////////////// Hard-fixing /////////////////////////////////////////
+
 int hard_fixing(instance *inst, double pfix);
 void fixing(CPXENVptr env, CPXLPptr lp, instance *inst, double pfix);
 void unfixing(CPXENVptr env, CPXLPptr lp, instance *inst);
+
+
+/////////////////////////////////// Local branching /////////////////////////////////////////
+
+int local_branching(instance *inst, int k);
+void add_local_branching_constraints(CPXENVptr env, CPXLPptr lp, instance *inst, int k);
+void remove_last_constraints(CPXENVptr env, CPXLPptr lp);
