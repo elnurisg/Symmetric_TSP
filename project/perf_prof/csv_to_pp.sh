4346d5b518b9d1346ab7f01726a041c568eba883
Elnur Isgandarov <elnur1isgenderov@gmail.com>
2024-05-06 14:46:55 +0200

    Add further performance analyze for Genetic Algorithm

--- project/perf_prof/csv_to_pp.sh
+++ project/perf_prof/csv_to_pp.sh
@@ -11,7 +11,8 @@ heuristics(){
 }
 
 metaheuristics(){
-    python3 ./perfprof.py -D , -T 600 -M 1.1 -X "Cost Ratio" ./metaheuristics/genetic_algorithm/genetic_algorithm.csv ./metaheuristics/genetic_algorithm/genetic_algorithm.pdf -P "10 instances for Genetic algorithm"
+    # do not perfprof genetic algo bcz it has too many parameters so we do deeper analyze
+    # python3 ./perfprof.py -D , -T 600 -M 1.1 -X "Cost Ratio" ./metaheuristics/genetic_algorithm/genetic_algorithm.csv ./metaheuristics/genetic_algorithm/genetic_algorithm.pdf -P "10 instances for Genetic algorithm"
 
     python3 ./perfprof.py -D , -T 600 -M 1.1 -X "Cost Ratio" ./metaheuristics/simulated_annealing/simulated_annealing.csv ./metaheuristics/simulated_annealing/simulated_annealing.pdf -P "10 instances for Simulated Annealing"
 
@@ -63,8 +64,76 @@ further_tabu() {
     python3 ./perfprof.py -D , -T 600 -M 1.1 -X "Cost Ratio" ./metaheuristics/tabu_search/tabu_with_aspiration_1.csv ./metaheuristics/tabu_search/tabu_with_aspiration_1.pdf -P "10 instances for Tabu Search without aspiration criteria"
 }
 
+genetic_analyze_cut() {
+    # Genetic input file path
+    input_file="./metaheuristics/genetic_algorithm/genetic_algorithm.csv"
+    directory=./metaheuristics/genetic_algorithm/cutting_analyze
+
+    mkdir -p $directory 
+    # output file paths
+    output_cutting_type_0="$directory/GA_cutting_type_0.csv"
+    output_cutting_type_1="$directory/GA_cutting_type_1.csv"
+    output_cutting_type_2="$directory/GA_cutting_type_2.csv"
+
+    cut -d, -f 1-2,3,6,9 $input_file > ./temp0.csv
+    cut -d, -f 1-2,4,7,10 $input_file > ./temp1.csv
+    cut -d, -f 1-2,5,8,11 $input_file > ./temp2.csv
+
+    sed '1 s/10/4/' ./temp0.csv > $output_cutting_type_0
+    sed '1 s/10/4/' ./temp1.csv > $output_cutting_type_1
+    sed '1 s/10/4/' ./temp2.csv > $output_cutting_type_2
+
+    rm ./temp0.csv
+    rm ./temp1.csv
+    rm ./temp2.csv
+
+}
+
+genetic_analyze_repair() {
+    # Genetic input file path
+    input_file="./metaheuristics/genetic_algorithm/genetic_algorithm.csv"
+    directory=./metaheuristics/genetic_algorithm/repair_analyze
+
+    mkdir -p $directory 
+    # output file paths
+    output_repair_mode_0="$directory/GA_repair_mode_0.csv"
+    output_repair_mode_1="$directory/GA_repair_mode_1.csv"
+    output_repair_mode_2="$directory/GA_repair_mode_2.csv"
+
+    cut -d, -f 1-2,3,4,5 $input_file > ./temp0.csv
+    cut -d, -f 1-2,6,7,8 $input_file > ./temp1.csv
+    cut -d, -f 1-2,9,10,11 $input_file > ./temp2.csv
+
+    sed '1 s/10/4/' ./temp0.csv > $output_repair_mode_0
+    sed '1 s/10/4/' ./temp1.csv > $output_repair_mode_1
+    sed '1 s/10/4/' ./temp2.csv > $output_repair_mode_2
+
+    rm ./temp0.csv
+    rm ./temp1.csv
+    rm ./temp2.csv
+
+}
+
+genetic_more_csv() {
+    genetic_analyze_cut
+    genetic_analyze_repair
+}
+
+further_genetic() {
+    genetic_more_csv
+    
+    python3 ./perfprof.py -D , -T 600 -M 1.17 -X "Cost Ratio" ./metaheuristics/genetic_algorithm/cutting_analyze/GA_cutting_type_0.csv ./metaheuristics/genetic_algorithm/cutting_analyze/GA_cuting_type_0.pdf -P "10 instances for GA with cutting from middle"
+    python3 ./perfprof.py -D , -T 600 -M 1.2 -X "Cost Ratio" ./metaheuristics/genetic_algorithm/cutting_analyze/GA_cutting_type_1.csv ./metaheuristics/genetic_algorithm/cutting_analyze/GA_cuting_type_1.pdf -P "10 instances for GA with cutting from random position"
+    python3 ./perfprof.py -D , -T 600 -M 1.2 -X "Cost Ratio" ./metaheuristics/genetic_algorithm/cutting_analyze/GA_cutting_type_2.csv ./metaheuristics/genetic_algorithm/cutting_analyze/GA_cutting_type_2.pdf -P "10 instances for GA with cutting from one-fourth of the tour"
+ 
+    python3 ./perfprof.py -D , -T 600 -M 1.18 -X "Cost Ratio" ./metaheuristics/genetic_algorithm/repair_analyze/GA_repair_mode_0.csv ./metaheuristics/genetic_algorithm/repair_analyze/GA_repair_mode_0.pdf -P "10 instances for GA without repair"
+    python3 ./perfprof.py -D , -T 600 -M 1.17 -X "Cost Ratio" ./metaheuristics/genetic_algorithm/repair_analyze/GA_repair_mode_1.csv ./metaheuristics/genetic_algorithm/repair_analyze/GA_repair_mode_1.pdf -P "10 instances for GA with repairing the bad genes"
+    python3 ./perfprof.py -D , -T 600 -M 1.12 -X "Cost Ratio" ./metaheuristics/genetic_algorithm/repair_analyze/GA_repair_mode_2.csv ./metaheuristics/genetic_algorithm/repair_analyze/GA_repair_mode_2.pdf -P "10 instances for GA with repairing the bad genes + two-opt refining"
+}
+
 # heuristics
 # metaheuristics_with_optimal
 # metaheuristics
 
-further_tabu
+# further_tabu
+further_genetic
